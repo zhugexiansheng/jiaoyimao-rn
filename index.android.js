459b5769a9adf45d5e7327fc389186406da7dba7
“zgx” <15876500359@163.com>
2016-06-19 20:04:10 +0800

    git push test

--- index.android.js
+++ index.android.js
@@ -16,7 +16,7 @@ class AwesomeProject extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          git test push ....
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
